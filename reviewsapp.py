1f4130229037df690ec1ee01b51f119a58e85928
Mac <mac@Macde-MacBook-Pro.local>
2020-04-11 16:28:24 +0800

    calc avg of reviews

--- reviewsapp.py
+++ reviewsapp.py
@@ -9,7 +9,12 @@ with open ('reviews.txt','r') as f :
 		if count % 100000 == 0 :
 			print (len(data)) 
 
+print('檔案讀取完畢，總共有', len(data) ,'筆資料')
 
-print(len(data))
 
+sum_len = 0
+for word in data :
+	sum_len = sum_len + len(word)
+
+print('每筆留言平均數是：' ,sum_len / len(data))
 
